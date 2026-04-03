61eafa6b9e2467595cb8698db72517380dd63d86
BasmaT <162066805+BasmaTarre@users.noreply.github.com>
2026-04-03 16:26:54 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+USE MASTER;
+GO
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name ='Datawarehouse')
+BEGIN
+    ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE ;
+    DROP DATABASE Datawarehouse;
+END;
+GO
+CREATE DATABASE Datawarehouse;
+GO
+USE  Datawarehouse;
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO 
+CREATE SCHEMA gold;
